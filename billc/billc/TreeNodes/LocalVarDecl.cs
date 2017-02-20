bdb58c741c23c2b3e6afafc3860688d7b4363b62
ajgajg1134 <ajgajg1134@gmail.com>
2017-02-20 17:48:29 -0500

    Adding local var declarations framework

---
+++ billc/billc/TreeNodes/LocalVarDecl.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace billc.TreeNodes
+{
+    class LocalVarDecl : Statement
+    {
+        string id;
+        Expression val;
+
+        public LocalVarDecl(string i, Expression v)
+        {
+            id = i;
+            val = v;
+        }
+    }
+}
