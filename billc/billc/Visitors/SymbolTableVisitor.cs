fbbfbb2c6355bf209ce692daf0e6e4d400306a3b
ajgajg1134 <ajgajg1134@gmail.com>
2017-02-21 18:54:18 -0500

    Fixing issue with visitor namespaces, adding visitor

---
+++ billc/billc/Visitors/SymbolTableVisitor.cs
@@ -0,0 +1,67 @@
+﻿using billc.TreeNodes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace billc.Visitors
+{
+    class SymbolTableVisitor : Visitor
+    {
+        public void visit(ClassDecl cdecl)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void visit(BinaryOperator bop)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void visit(FormalParam fparam)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void visit(LocalVarDecl ldecl)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void visit(UnaryOperator unop)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void visit(Conditional cond)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void visit(Expression exp)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void visit(Literal literal)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void visit(Assignment astmt)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void visit(FunctionDecl fdecl)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void visit(ProgramNode node)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
