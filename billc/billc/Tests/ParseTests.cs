ac5c4588f63ebf2c7f46b5458bd1dc22c6793509
ajgajg1134 <ajgajg1134@gmail.com>
2017-03-13 17:21:26 -0400

    We got tests! Added nunit first test

---
+++ billc/billc/Tests/ParseTests.cs
@@ -0,0 +1,37 @@
+﻿using billc.TreeNodes;
+using com.calitha.goldparser;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace billc.Tests
+{
+    [TestFixture]
+    class ParseTests
+    {
+        MyParser parser;
+
+        [SetUp]
+        protected void SetUp()
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+            using (var stream = assembly.GetManifestResourceStream("billc.Bill_Grammar.cgt"))
+            {
+                parser = new MyParser(stream);
+            }
+        }
+
+        [Test]
+        public void EmptyMain()
+        {
+            string prgrm = "void main() {}";
+            object parseRet = parser.Parse(prgrm);
+            Assert.IsNotNull(parseRet);
+            Assert.IsInstanceOf<ProgramNode>(parseRet);
+        }
+    }
+}
